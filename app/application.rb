544c2fd267aa668252f8ffb0b5e920c55af80e3d
Tom Banish <tombanish@gmail.com>
2020-06-21 20:06:46 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
     
     if req.path.match(/items/)
       
-      item = req.path.split("/items/").last
+      item_name = req.path.split("/items/").last
       song = @@songs.find{|s| s.title == song_title}
  
       resp.write song.artist
