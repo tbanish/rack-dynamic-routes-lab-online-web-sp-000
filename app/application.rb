1dab22e3716d6531a2296204d329f8ef6874b999
Tom Banish <tombanish@gmail.com>
2020-06-21 19:44:36 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,13 @@
 class Application
   
   resp = Rack::Response.new
+  req = Rack::Request.new
   
   def call(env)
     if req.path.match(/items/)
-    
+      @@items.each do |i|
+        
+    end
   end
   
 end
