ce321303b596d33c4b626874b1bc552e3df30a24
Tom Banish <tombanish@gmail.com>
2020-06-21 20:09:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,11 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
  
-      resp.write item.price
+      if @@items.include?(item)
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
       
     else
       resp.write "Route not found"
