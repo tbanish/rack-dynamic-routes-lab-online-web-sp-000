a1af70a8469dbb2cf3341983d23651f7022a39ec
Tom Banish <tombanish@gmail.com>
2020-06-21 20:08:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match(/items/)
       
       item_name = req.path.split("/items/").last
-      song = @@items.find{|i| i.name == item_name}
+      item = @@items.find{|i| i.name == item_name}
  
       resp.write item.price
       
